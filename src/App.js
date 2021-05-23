4036d6ce01630582619910688fc8e16c2ddfacbd
Sandro Soncini <sandrosoncini@hotmail.com>
2021-05-23 13:59:32 -0700

    Revert "added use-state"
    This reverts commit 37f4fb27f367f3be6b4964c4ebba4b30ec8a2929.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React, {useState} from 'react'
+
 import './App.css';
 
 function App() {
-
-  const [movies, setMovie] = useState(['Movie 1', 'Movie 2']);
-
-
   return (
     <div className="App">
       <header className="App-header">
         <h1>Movie Rater</h1>
       </header>
       <div className="layout">
-        <div>
-          {movies.map(movie => {
-            return (
-              <h2>{movie}</h2>
-            )
-          })}  
-        </div>
+        <div>Movie List</div>
         <div>Movie Details</div>
       </div>
     </div>
