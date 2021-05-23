1551ed30afdb3ca5e216e8aecc868a03a4852e91
Sandro Soncini <sandrosoncini@hotmail.com>
2021-05-23 14:08:29 -0700

    initialize useState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+
+  const [movies, setMovies] = useState(['Movie 1', 'Movie 2']);
   return (
     <div className="App">
       <header className="App-header">
         <h1>Movie Rater</h1>
       </header>
       <div className="layout">
-        <div>Movie List</div>
+        <div>
+          {movies.map( movie => {
+            return (<h2>{movie}</h2>)
+          })}
+        </div>
         <div>Movie Details</div>
       </div>
     </div>
