e7e4c22604f0cda06e609466ab93cb4364a4d7ff
Sandro Soncini <sandrosoncini@hotmail.com>
2021-05-23 14:24:10 -0700

    Fetching data fromApi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css';
 
 function App() {
 
-  const [movies, setMovies] = useState(['Movie 1', 'Movie 2']);
+  const [movies, setMovie] = useState([]);
+
+  useEffect (() => {
+    fetch("http://127.0.0.1:8000/api/movies", {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token a86dae480928e0f74b67181f418218f20fa41398'
+      }
+    })
+    .then (resp => resp.json())
+    .then (resp => setMovies(resp))
+    .catch (error => console.log(error))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
