37f4fb27f367f3be6b4964c4ebba4b30ec8a2929
Sandro Soncini <sandrosoncini@hotmail.com>
2021-05-23 13:57:43 -0700

    added use-state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-
+import React, {useState} from 'react'
 import './App.css';
 
 function App() {
+
+  const [movies, setMovie] = useState(['Movie 1', 'Movie 2']);
+
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Movie Rater</h1>
       </header>
       <div className="layout">
-        <div>Movie List</div>
+        <div>
+          {movies.map(movie => {
+            return (
+              <h2>{movie}</h2>
+            )
+          })}  
+        </div>
         <div>Movie Details</div>
       </div>
     </div>
